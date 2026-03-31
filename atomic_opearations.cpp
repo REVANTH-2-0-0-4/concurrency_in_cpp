e64a8500f5ce06be7b7bf00eae3f4b3fc7c56f5b
REVANTH-2-0-0-4 <revanthkurapati56@gmail.com>
2026-03-31 20:40:16 +0530

    progress atomic operations

--- atomic_opearations.cpp
+++ atomic_opearations.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <thread>
 #include <atomic>
-#include<vector>
-#include<jthread>
+#include <vector>
+
 using namespace std;
+
 atomic<int> shared_value(0);
+
 void increment() {
     shared_value++; // this is an atomic operations 
-    // shared_value = shared_value + 1 --> this is a non atomic operation ,
+    // shared_value = shared_value + 1 --> this is a non atomic operation
+    // similarly shared_value+=4; is an atomic operator += , &= , |= , -= , all these are atomic operations 
 }
+
 int main() {
-   vector<jthread> threads;
-   for(int i = 0 ; i < 10 ; i++){
-    jthreads.push_back(jthread(increment));  
-   }
+    vector<jthread> threads;
+
+    for(int i = 0 ; i < 10 ; i++){
+        threads.push_back(jthread(increment));  
+    }
    
     cout << shared_value << endl;
 }
+
+ // this atomic , can also work with the custom data types , given that, they are trivially copyable ( no non primitives or the custom constrcutors);
+ // atomic operations are hardware level privilages ,that we are getting , it happens only when the operation is single step ( that is when the data is trivially copyable)
